def7e58b3c42164c46aea635de53fd33f6541e93
Luis Pereira <lfbp@academy.cin.ufpe.br>
2022-04-21 16:09:50 -0300

    sender state machine without timer

--- MainClient.py
+++ MainClient.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import time
 
 FORMAT = "utf-8"
 PORT = 5050
@@ -9,12 +10,15 @@ ADDR = (SERVER, PORT)
 WAIT_CALL_0 = 0
 WAIT_ACK_0 = 1
 WAIT_CALL_1 = 2
+WAIT_ACK_1 = 3
 ACK = "ACK"
+NACK = "NACK"
+initialTime = time.perf_counter()
 
 sequentialNumber = 0
 currentState = WAIT_CALL_0
 
-def checksum(data):
+def generateCheckum(data):
     sum = 0
     for i in range(0,len(data),2):
         if i + 1 < len(data):
@@ -40,33 +44,76 @@ def getSequencialNumber():
     sequentialNumber = (sequentialNumber + 1) % 2
     return number
 
-def send(message):
+def send(message, addr):
     packet = message.encode()
     datalen = len(packet)
-    checksumValue = checksum(packet)
+    checksumValue = generateCheckum(packet)
     sequentialNumber = getSequencialNumber()
     header = struct.pack("!III", datalen, checksumValue,sequentialNumber)
     headerPlusMessage = header + packet
-    client.sendto(headerPlusMessage, ADDR)
+    client.sendto(headerPlusMessage, addr)
 
+def isACK(ack):
+    return ack == ACK
+
+def decodeData(fullPacket):
+    udpHeader = fullPacket[:12]
+    data = fullPacket[12:]
+    header = struct.unpack("!III", udpHeader)
+    return data, header
+
+def corrupted(data, checksum):
+    sum = 0
+    for i in range(0,len(data),2):
+        if i + 1 < len(data):
+            byteValue = bytearray(2)
+            byteValue[0] = (data[i]) 
+            byteValue[1] = (data[i+1])
+            value = int.from_bytes(byteValue, "big")             
+            sum = (sum + value) % 65535
+        else:
+            byteValue = bytearray(2)
+            byteValue[0] = (data[i]) 
+            byteValue[1] = 0
+            value = int.from_bytes(byteValue, "big")            
+            sum = (sum + value) % 65535
+    sum = (sum + checksum)
+    return sum != int('0xFFFF', 0)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     if currentState == WAIT_CALL_0:
         message = input("send message: ")
-        send(message)
+        send(message, ADDR)
         currentState = WAIT_ACK_0
     elif currentState == WAIT_ACK_0:
-        print("waiting for ACK 0")
-        ack, conn = client.recvfrom(BUFFER_SIZE)
-        ack = str(ack.decode(FORMAT))
-        if ack == ACK:
-            print("ACK RECIEVED")
+        print("waiting for ACK 0...")
+        client.settimeout(1.0)
+        fullPacket, conn = client.recvfrom(BUFFER_SIZE)
+        data, header = decodeData(fullPacket)
+        checksum = header[1]
+        seq = header[2]
+        ack = data.decode(FORMAT)
+        if isACK(ack) and not corrupted(data, checksum) and seq == 0:
+            print("ACK 0 RECIEVED")
+            #stop timer
             currentState = WAIT_CALL_1
+            continue
     elif currentState == WAIT_CALL_1:
-        print("implement wait for above call 1")
-        break
-
-
-
+        message = input("send message: ")
+        send(message, ADDR)
+        initialTime = time.perf_counter()
+        currentState = WAIT_ACK_1
+    elif currentState == WAIT_ACK_1:
+        print("waiting for ACK 1...")
+        fullPacket, conn = client.recvfrom(BUFFER_SIZE)
+        data, header = decodeData(fullPacket)
+        checksum = header[1]
+        seq = header[2]
+        ack = data.decode(FORMAT)
+        if isACK(ack) and not corrupted(data, checksum) and seq == 1:
+            print("ACK 1 RECIEVED")
+            #stop timer
+            currentState = WAIT_CALL_0
+            continue
--- MainServer.py
+++ MainServer.py
@@ -8,8 +8,9 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 ACK = "ACK"
+sequentialNumber = 0
 
-def isCorrupted(data, checksum):
+def corrupted(data, checksum):
     sum = 0
     for i in range(0,len(data),2):
         if i + 1 < len(data):
@@ -25,17 +26,56 @@ def isCorrupted(data, checksum):
             value = int.from_bytes(byteValue, "big")            
             sum = (sum + value) % 65535
     sum = (sum + checksum)
+    return sum != int('0xFFFF', 0)
+
+def generateCheckum(data):
+    sum = 0
+    for i in range(0,len(data),2):
+        if i + 1 < len(data):
+            byteValue = bytearray(2)
+            byteValue[0] = (data[i]) 
+            byteValue[1] = (data[i+1])
+            value = int.from_bytes(byteValue, "big")           
+            sum = (sum + value) % 65535
+        else:
+            byteValue = bytearray(2)
+            byteValue[0] = (data[i]) 
+            byteValue[1] = 0
+            value = int.from_bytes(byteValue, "big")            
+            sum = (sum + value) % 65535
+    sum = ~sum
+    sum = (sum + 2**32) % 65535
     return sum 
 
-def handleClient(msg, clientCheckSum, sequentialNumber, conn):
-    print("Message from client: " + str(msg.decode(FORMAT)))
+def handleClient(msg, clientCheckSum, sequentialNumber, addr):
+    print("Message from client: " + msg.decode(FORMAT))
     print(f'Message sequential number: {sequentialNumber}')
-    checkSum = isCorrupted(msg, clientCheckSum)
-    if checkSum == int('0xFFFF', 0):
-        print("DATA NOT CORRUPTED, SENDING ACK...")
-        server.sendto(ACK.encode(), conn)
+    corrupted(msg, clientCheckSum)
+    if not corrupted(msg, clientCheckSum):
+        send(ACK, addr)
     else:
         print("CORRUPTED")
+
+def getSequencialNumber():
+    global sequentialNumber
+    number = sequentialNumber
+    sequentialNumber = (sequentialNumber + 1) % 2
+    return number
+
+def send(message, addr):
+    packet = message.encode()
+    datalen = len(packet)
+    checksumValue = generateCheckum(packet)
+    sequentialNumber = getSequencialNumber()
+    header = struct.pack("!III", datalen, checksumValue,sequentialNumber)
+    headerPlusMessage = header + packet
+    server.sendto(headerPlusMessage, addr)
+
+def decodeData(fullPacket):
+    udpHeader = fullPacket[:12]
+    data = fullPacket[12:]
+    header = struct.unpack("!III", udpHeader)
+    return data, header
 
 
 print("Server ip is " + SERVER)
@@ -45,11 +85,13 @@ print("Binding to " + str(ADDR[0]) + ", " + str(ADDR[1]))
 server.bind(ADDR)
 
 while True:
+    print("")
     print("Waiting client connction...")
-    full_packet, conn = server.recvfrom(BUFFER_SIZE)
-    udp_header = full_packet[:12]
-    data = full_packet[12:]
-    udpHeader = struct.unpack("!III", udp_header)
+    fullPacket, conn = server.recvfrom(BUFFER_SIZE)
+    data, header = decodeData(fullPacket)
+    checksum = header[1]
+    sequentialNumber = header[2]
     # newThead = threading.Thread(target=handleClient, args=(data, udpHeader[1], udpHeader[2], conn))
     # newThead.start()
-    handleClient(data, udpHeader[1], udpHeader[2], conn)
+    handleClient(data, checksum, sequentialNumber, conn)
+    print("")
