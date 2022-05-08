57d71fada6d6d5a7411bec86be62048cc3d1f8ea
Michael <michaelbarneyjunior@gmail.com>
2022-05-08 19:27:57 -0300

    feat: established 2 way communication

--- MainClient.py
+++ MainClient.py
@@ -1,37 +1,79 @@
 import socket
+import threading
 import struct
-import time
 import common
 
-FORMAT = "utf-8"
-PORT = 5050
+SERVER_PORT = 8080
+CLIENT_PORT = 5050
+
 BUFFER_SIZE = 2048
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
+HOST_NAME = socket.gethostbyname(socket.gethostname())
+CLIENT_ADDR = (HOST_NAME, CLIENT_PORT)
+SERVER_ADDR = (HOST_NAME, SERVER_PORT)
+
+FORMAT = "utf-8"
+ACK = "ACK"
+
+expected_sequence = 0
+last_acked_sequence = 2
 
 WAIT_CALL = 0
 WAIT_ACK = 1
 TIMEOUT = 2
 CORRUPT = 3
 DUPLICATE_ACK = 4
-
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.settimeout(1.0)
+WAIT_RESPONSE = 5
 
 sent_message = ""
 current_sequence = 0
-current_state = WAIT_CALL
+current_state = WAIT_CALL#WAIT_RESPONSE
 
+print("Server ip is " + HOST_NAME)
+print("Creating socket...")
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("Binding to " + str(CLIENT_ADDR[0]) + ", " + str(CLIENT_ADDR[1]))
+server.bind(CLIENT_ADDR)
 
 # Aux Functions
-
 def send(message, addr):
     packet = message.encode()
     datalen = len(packet)
     checksumValue = common.generateCheckum(packet)
     header = struct.pack("!III", datalen, checksumValue, current_sequence)
     headerPlusMessage = header + packet
-    client.sendto(headerPlusMessage, addr)
+    print("Send to ", addr)
+    server.sendto(headerPlusMessage, addr)
+    print("Sent")
+
+
+def handleClient(msg, clientCheckSum, sequence_number, addr):
+    global expected_sequence
+    global last_acked_sequence
+
+    print("Message from client: " + msg.decode(FORMAT))
+    print(f'Received Sequence: {sequence_number}')
+    print(f'Expected Sequence: {expected_sequence}')
+
+    if common.corrupted(msg, clientCheckSum):
+        print("CORRUPTED, ACK Previous: ", last_acked_sequence)
+        sendACK(addr, last_acked_sequence)
+
+    elif sequence_number != expected_sequence:
+        print("Unexpected sequence number")
+        sendACK(addr, last_acked_sequence)
+    
+    else:
+        sendACK(addr, expected_sequence)
+        last_acked_sequence = sequence_number
+        expected_sequence = (sequence_number + 1) % 2
+
+def sendACK(addr, sequence_number):
+    packet = "ACK".encode()
+    datalen = len(packet)
+    checksumValue = common.generateCheckum(packet)
+    header = struct.pack("!III", datalen, checksumValue, sequence_number)
+    headerPlusMessage = header + packet
+    server.sendto(headerPlusMessage, addr)
 
 def decodeData(fullPacket):
     udpHeader = fullPacket[:12]
@@ -39,17 +81,25 @@ def decodeData(fullPacket):
     header = struct.unpack("!III", udpHeader)
     return data, header
 
-# Main Loop
 while True:
     if current_state == WAIT_CALL:
         sent_message = input("Send message: ")
-        send(sent_message, ADDR)
+        send(sent_message, SERVER_ADDR)
         current_state = WAIT_ACK
+
+    elif current_state == WAIT_RESPONSE:
+        print("\nWaiting server connection...")
+        fullPacket, conn = server.recvfrom(BUFFER_SIZE)
+        data, header = decodeData(fullPacket)
+        checksum = header[1]
+        sequence_number = header[2]
+        handleClient(data, checksum, sequence_number, conn)
+        current_state = WAIT_CALL
 
     elif current_state == WAIT_ACK:
         print("waiting for ACK " + str(current_sequence) + "...")
         try:
-            fullPacket, conn = client.recvfrom(BUFFER_SIZE)
+            fullPacket, conn = server.recvfrom(BUFFER_SIZE)
             data, header = decodeData(fullPacket)
             checksum = header[1]
             seq = header[2]
@@ -63,7 +113,7 @@ while True:
 
             else:
                 print("ACK " + str(current_sequence) + " RECIEVED")
-                current_state = WAIT_CALL
+                current_state = WAIT_RESPONSE
                 current_sequence = (current_sequence + 1) % 2
                 continue    
         except socket.timeout as e:
@@ -71,15 +121,15 @@ while True:
 
     elif current_state == TIMEOUT:
         print("Timed Out, ACK " + str(current_sequence) + " not received")
-        send(sent_message, ADDR)
+        send(sent_message, SERVER_ADDR)
         print("Message Recent")
         current_state = WAIT_ACK
 
     elif current_state == CORRUPT:
-        print("Corrupt Message")
-        send(sent_message, ADDR)
+        print("Corrupt Message") #dbg
+        send(sent_message, SERVER_ADDR)
         print("Message Recent")
-        current_state = WAIT_ACK
+        current_state = WAIT_ACK #dbg
 
     elif current_state == DUPLICATE_ACK:
         print("Incorrect Sequence Received")
--- MainServer.py
+++ MainServer.py
@@ -3,23 +3,49 @@ import threading
 import struct
 import common
 
-PORT = 5050
+SERVER_PORT = 8080
+CLIENT_PORT = 5050
+
 BUFFER_SIZE = 2048
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
+HOST_NAME = socket.gethostbyname(socket.gethostname())
+CLIENT_ADDR = (HOST_NAME, CLIENT_PORT)
+SERVER_ADDR = (HOST_NAME, SERVER_PORT)
+
 FORMAT = "utf-8"
 ACK = "ACK"
 
 expected_sequence = 0
 last_acked_sequence = 2
 
-print("Server ip is " + SERVER)
+WAIT_CALL = 0
+WAIT_ACK = 1
+TIMEOUT = 2
+CORRUPT = 3
+DUPLICATE_ACK = 4
+WAIT_RESPONSE = 5
+
+sent_message = ""
+current_sequence = 0
+current_state = WAIT_RESPONSE
+
+print("Server ip is " + HOST_NAME)
 print("Creating socket...")
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print("Binding to " + str(ADDR[0]) + ", " + str(ADDR[1]))
-server.bind(ADDR)
+print("Binding to " + str(SERVER_ADDR[0]) + ", " + str(SERVER_ADDR[1]))
+server.bind(SERVER_ADDR)
 
 # Aux Functions
+def send(message, addr):
+    packet = message.encode()
+    datalen = len(packet)
+    checksumValue = common.generateCheckum(packet)
+    header = struct.pack("!III", datalen, checksumValue, current_sequence)
+    headerPlusMessage = header + packet
+    print("Send to ", addr)
+    server.sendto(headerPlusMessage, addr)
+    print("Sent")
+
+
 def handleClient(msg, clientCheckSum, sequence_number, addr):
     global expected_sequence
     global last_acked_sequence
@@ -58,12 +84,56 @@ def decodeData(fullPacket):
     header = struct.unpack("!III", udpHeader)
     return data, header
 
-
-# Main loop
 while True:
-    print("\nWaiting client connection...")
-    fullPacket, conn = server.recvfrom(BUFFER_SIZE)
-    data, header = decodeData(fullPacket)
-    checksum = header[1]
-    sequence_number = header[2]
-    handleClient(data, checksum, sequence_number, conn)
+    if current_state == WAIT_CALL:
+        sent_message = input("Send message: ")
+        send(sent_message, CLIENT_ADDR)
+        current_state = WAIT_ACK
+
+    elif current_state == WAIT_RESPONSE:
+        print("\nWaiting client connection...")
+        fullPacket, conn = server.recvfrom(BUFFER_SIZE)
+        data, header = decodeData(fullPacket)
+        checksum = header[1]
+        sequence_number = header[2]
+        handleClient(data, checksum, sequence_number, conn)
+        current_state = WAIT_CALL
+
+    elif current_state == WAIT_ACK:
+        print("waiting for ACK " + str(current_sequence) + "...")
+        try:
+            fullPacket, conn = server.recvfrom(BUFFER_SIZE)
+            data, header = decodeData(fullPacket)
+            checksum = header[1]
+            seq = header[2]
+            message = data.decode(FORMAT)
+
+            if message != "ACK" or common.corrupted(data, checksum):                
+                current_state = CORRUPT
+
+            elif seq != current_sequence:
+                current_state = DUPLICATE_ACK
+
+            else:
+                print("ACK " + str(current_sequence) + " RECIEVED")
+                current_state = WAIT_RESPONSE
+                current_sequence = (current_sequence + 1) % 2
+                continue    
+        except socket.timeout as e:
+            current_state = TIMEOUT
+
+    elif current_state == TIMEOUT:
+        print("Timed Out, ACK " + str(current_sequence) + " not received")
+        send(sent_message, CLIENT_ADDR)
+        print("Message Recent")
+        current_state = WAIT_ACK
+
+    elif current_state == CORRUPT:
+        print("Corrupt Message") #dbg
+        send(sent_message, CLIENT_ADDR)
+        print("Message Recent")
+        current_state = WAIT_ACK #dbg
+
+    elif current_state == DUPLICATE_ACK:
+        print("Incorrect Sequence Received")
+        current_state = WAIT_ACK 
